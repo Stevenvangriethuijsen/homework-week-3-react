917ecba040184a1eda6321b034557cd47b49eb31
Stevenvangriethuijsen <Stevenvangriethuijsen@Outlook.com>
2020-02-15 14:57:48 +0100

    added componentDidmount and fetched all quotes with keyword tree

--- src/components/QuoteSearcher.js
+++ src/components/QuoteSearcher.js
@@ -6,8 +6,20 @@ export default class QuoteSearcher extends React.Component {
     quotes: []
   };
 
+  componentDidMount = async () => {
+    const response = await fetch(
+      "https://quote-garden.herokuapp.com/quotes/search/tree"
+    );
+    const parsedResponse = await response.json();
+    console.log("parsedresponde from Cdidmount", parsedResponse);
+    const quotesList = parsedResponse.results;
+    console.log("quoteslist from cdidmount", quotesList);
+    this.setState({ loading: false, quotes: quotesList });
+  };
   render() {
+    console.log("logging state quotes from render", this.state.quotes);
     return this.state.quotes.map(object => {
+      console.log(object);
       return (
         <Quote
           text={object.quoteText}
