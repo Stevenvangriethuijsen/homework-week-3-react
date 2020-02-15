8d1725a685dae1f0c2a741107a6d747e560a7004
Stevenvangriethuijsen <Stevenvangriethuijsen@Outlook.com>
2020-02-15 20:10:46 +0100

    added working search bar and loading screen still functional

--- src/components/QuoteSearcher.js
+++ src/components/QuoteSearcher.js
@@ -5,36 +5,77 @@ export default class QuoteSearcher extends React.Component {
   state = {
     quotes: [],
     count: 0,
-    fetching: true
+    fetching: false,
+    searchParam: ""
   };
 
-  componentDidMount = async () => {
-    const response = await fetch(
-      "https://quote-garden.herokuapp.com/quotes/search/tree"
-    );
-    const parsedResponse = await response.json();
-    // console.log("parsedresponde from Cdidmount", parsedResponse);
-    const quotesList = parsedResponse.results;
-    const quotesCount = parsedResponse.count;
-    // console.log("quoteslist from cdidmount", quotesList);
-    const quotes = quotesList.map(quote => {
-      return {
-        ...quote,
-        quoteStyling: "nostyle"
-      };
-    });
-    this.setState({ fetching: false, quotes: quotes, count: quotesCount });
+  // componentDidMount = async () => {
+  //   const response = await fetch(
+  //     "https://quote-garden.herokuapp.com/quotes/search/tree"
+  //   );
+  //   const parsedResponse = await response.json();
+  //   console.log("parsedresponde from Cdidmount", parsedResponse);
+  //   const quotesList = parsedResponse.results;
+  //   const quotesCount = parsedResponse.count;
+  //   // console.log("quoteslist from cdidmount", quotesList);
+  //   const quotes = quotesList.map(quote => {
+  //     return {
+  //       ...quote,
+  //       quoteStyling: "nostyle"
+  //     };
+  //   });
+  //   this.setState({ fetching: false, quotes: quotes, count: quotesCount });
+  // };
+
+  handleChange = event => {
+    console.log(event.target.value);
+    this.setState({ searchParam: event.target.value });
   };
+
+  handleSubmit = event => {
+    this.setState({ fetching: true });
+    console.log("is handle submit being called");
+    event.preventDefault();
+    return fetch(
+      `https://quote-garden.herokuapp.com/quotes/search/${this.state.searchParam}`
+    )
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+        const quoteList = data.results;
+        const quoteCount = data.count;
+        const quoteandstyle = quoteList.map(quote => {
+          return {
+            ...quote,
+            quoteStyling: "nostyle"
+          };
+        });
+        this.setState({
+          fetching: false,
+          quotes: quoteandstyle,
+          count: quoteCount
+        });
+      });
+  };
+
   render() {
     if (this.state.fetching) {
       return <h3>Loading...</h3>;
-    } else if (this.state.count === 0) {
-      return <h3>Nothing found that matches your parameters</h3>;
     } else {
       // console.log("logging state quotes from render", this.state.quotes);
       return (
         <div>
           <div>
+            <form onSubmit={this.handleSubmit}>
+              <label>
+                <input type="text" onChange={this.handleChange} />
+              </label>
+              <input
+                type="submit"
+                value="Search!"
+                onClick={this.handleSubmit}
+              />
+            </form>
             <h3>Liked: ... / Disliked: ...</h3>
           </div>{" "}
           {this.state.quotes.map(object => {
