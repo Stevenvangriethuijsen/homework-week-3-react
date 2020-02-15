edcd0ccbf48f8d54574b5764925f60396807dc85
Stevenvangriethuijsen <Stevenvangriethuijsen@Outlook.com>
2020-02-15 17:01:47 +0100

    added functionality to buttons and quotestyling property to quote class finished step 4

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -3,18 +3,25 @@ import "./Quote.css";
 
 export default class Quote extends React.Component {
   state = {
-    numlikes: 0
+    quoteStyling: "nostyle"
+  };
+
+  dislike = () => {
+    this.setState({ quoteStyling: "disliked" });
+  };
+  like = () => {
+    this.setState({ quoteStyling: "liked" });
   };
   render() {
     return (
       <div className="quoteWrapper">
         <div className="quoteText">
-          <p>{this.props.text}</p>
+          <p className={this.state.quoteStyling}>{this.props.text}</p>
         </div>
         <div className="quoteAuthor">
           By: {this.props.author}
-          <button>:)</button>
-          <button>:(</button>
+          <button onClick={this.like}>:)</button>
+          <button onClick={this.dislike}>:(</button>
         </div>
       </div>
     );
